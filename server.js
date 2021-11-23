1c94031208f2922ff6efdbaa7db531f6ba01125e
William Wang <billwang115@gmail.com>
2021-11-23 16:38:46 -0500

    fixed express bug

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.use(express.static(path.join(__dirname, "/pub")));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  log(`Listening on port ${port}...`);
+  console.log(`Listening on port ${port}...`);
 }); // localhost development port 5000  (http://localhost:5000)
